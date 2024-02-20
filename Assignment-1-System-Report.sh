985f102e371e5bff16c464fc5b0f7ba6b30a9c7c
shreypatel1702 <157723420+shreypatel1702@users.noreply.github.com>
2024-02-20 12:08:17 -0500

    Update Assignment-1-System-Report.sh

--- Assignment-1-System-Report.sh
+++ Assignment-1-System-Report.sh
@@ -6,25 +6,28 @@ uptime=$(uptime -p)
 os=$(source /etc/os-release && echo $PRETTY_NAME)
 
 # Gather Hardware Information
-cpu=$(sudo lshw -class processor | grep "product" | head -1)
-ram=$(free -h | grep Mem | awk '{print $2}')
-disks=$(lsblk -d | awk '{print $1, $4}')
 video=$(sudo lshw -class display | grep "product")
+cpu=$(sudo lshw -class processor | grep "product" | head -1)
+disks=$(lsblk -d | awk '{print $1, $4}')
+ram=$(free -h | grep Mem | awk '{print $2}')
+
 
 # Gather Network Information
-ip=$(hostname -I | awk '{print $1}')
-gateway=$(ip r | grep default | awk '{print $3}')
-dns=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}')
 fqdn=$(hostname -f)
+ip=$(hostname -I | awk '{print $1}')
+dns=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}')
+gateway=$(ip r | grep default | awk '{print $3}')
+
+
 
 # Gather System Status
 users=$(who | awk '{print $1}' | sort | uniq)
 disk_space=$(df -h)
+memory_allocation=$(free -h)
 process_count=$(ps -e | wc -l)
 load_average=$(uptime | awk -F'[a-z]:' '{ print $2}')
-memory_allocation=$(free -h)
-listening_ports=$(ss -tulwn | grep LISTEN)
 ufw_rules=$(sudo ufw status verbose)
+listening_ports=$(ss -tulwn | grep LISTEN)
 
 # Print Report
 
