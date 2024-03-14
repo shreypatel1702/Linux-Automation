181f7ebf4019a77ff93599d2f4e51270afd5d407
shreypatel1702 <shreypatel7265@gmail.com>
2024-03-14 18:26:18 -0400

    Assignment2.sh

--- Assignment2.sh
+++ Assignment2.sh
@@ -1,1 +1,7 @@
 wget -O ~/makecontainers.sh https://zonzorp.github.io/COMP2137/makecontainers.sh && chmod +x ~/makecontainers.sh
+shrey@Generic-VM:~/Linux-Automation$ wget -O ~/makecontainers.sh https://zonzorp.github.io/COMP2137/makecontainers.sh && chmod +x ~/makecontainers.sh
+--2024-03-14 18:23:45--  https://zonzorp.github.io/COMP2137/makecontainers.sh
+Resolving zonzorp.github.io (zonzorp.github.io)... 185.199.111.153, 185.199.108.153, 185.199.109.153, ...
+Connecting to zonzorp.github.io (zonzorp.github.io)|185.199.111.153|:443... connected.
+HTTP request sent, awaiting response... 404 Not Found
+2024-03-14 18:23:45 ERROR 404: Not Found.
