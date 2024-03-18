e2ff689528056359e41adfa096fb48776a04d2d9
shreypatel1702 <shreypatel7265@gmail.com>
2024-03-17 22:34:28 -0400

    Error.sh

--- Error.sh
+++ Error.sh
@@ -1,5 +1,6 @@
-wget -O ~/check-assign2-script https://zonzorp.github.io/COMP2137/check-assign2-script &&
-
-chmod +x ~/check-assign2-script &&
-
 ~/check-assign2-script https://github.com/yourgithubusername/yourreponame/assignment2.sh 2>&1 >~/assignment2-checked.txt
+--2024-03-17 22:33:32--  https://zonzorp.github.io/COMP2137/check-assign2-script
+Resolving zonzorp.github.io (zonzorp.github.io)... 185.199.110.153, 185.199.111.153, 185.199.108.153, ...
+Connecting to zonzorp.github.io (zonzorp.github.io)|185.199.110.153|:443... connected.
+HTTP request sent, awaiting response... 404 Not Found
+2024-03-17 22:33:33 ERROR 404: Not Found.
