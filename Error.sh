aea03f05d0091d8f3be5a5760f8f0b32001248c8
shreypatel1702 <157723420+shreypatel1702@users.noreply.github.com>
2024-03-17 22:32:26 -0400

    Update Error.sh

--- Error.sh
+++ Error.sh
@@ -1,33 +1,5 @@
-INFO: User dennis already exists
-cp: cannot stat '/.ssh/authorized_keys': No such file or directory
-chmod: cannot access '/home/dennis/.ssh/authorized_keys': No such file or directory
-INFO: User aubrey already exists
-cp: cannot stat '/.ssh/authorized_keys': No such file or directory
-chmod: cannot access '/home/aubrey/.ssh/authorized_keys': No such file or directory
-INFO: User captain already exists
-cp: cannot stat '/.ssh/authorized_keys': No such file or directory
-chmod: cannot access '/home/captain/.ssh/authorized_keys': No such file or directory
-INFO: User snibbles already exists
-cp: cannot stat '/.ssh/authorized_keys': No such file or directory
-chmod: cannot access '/home/snibbles/.ssh/authorized_keys': No such file or directory
-INFO: User brownie already exists
-cp: cannot stat '/.ssh/authorized_keys': No such file or directory
-chmod: cannot access '/home/brownie/.ssh/authorized_keys': No such file or directory
-INFO: User scooter already exists
-cp: cannot stat '/.ssh/authorized_keys': No such file or directory
-chmod: cannot access '/home/scooter/.ssh/authorized_keys': No such file or directory
-INFO: User sandy already exists
-cp: cannot stat '/.ssh/authorized_keys': No such file or directory
-chmod: cannot access '/home/sandy/.ssh/authorized_keys': No such file or directory
-INFO: User perrier already exists
-cp: cannot stat '/.ssh/authorized_keys': No such file or directory
-chmod: cannot access '/home/perrier/.ssh/authorized_keys': No such file or directory
-INFO: User cindy already exists
-cp: cannot stat '/.ssh/authorized_keys': No such file or directory
-chmod: cannot access '/home/cindy/.ssh/authorized_keys': No such file or directory
-INFO: User tiger already exists
-cp: cannot stat '/.ssh/authorized_keys': No such file or directory
-chmod: cannot access '/home/tiger/.ssh/authorized_keys': No such file or directory
-INFO: User yoda already exists
-cp: cannot stat '/.ssh/authorized_keys': No such file or directory
-chmod: cannot access '/home/yoda/.ssh/authorized_keys': No such file or directory
+wget -O ~/check-assign2-script https://zonzorp.github.io/COMP2137/check-assign2-script &&
+
+chmod +x ~/check-assign2-script &&
+
+~/check-assign2-script https://github.com/yourgithubusername/yourreponame/assignment2.sh 2>&1 >~/assignment2-checked.txt
