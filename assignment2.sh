80eb04f902ce175adbfb0896bc07475a51f7aac9
shreypatel1702 <157723420+shreypatel1702@users.noreply.github.com>
2024-03-19 22:37:29 -0400

    Update assignment2.sh

--- assignment2.sh
+++ assignment2.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Network Interface Configuration
+
 echo "WE WILL NOW CONFIGURE NETWORK CONFIGURATION"
+echo "--------------------------------------------------------------------------------------------------------------------------------"
 sudo bash -c 'cat << EOF > /etc/netplan/01-netcfg.yaml
 network:
   version: 2
@@ -19,15 +21,18 @@ sudo netplan apply
 
 # Hosts File Update
 echo "WE WILL NOW UPDATE /ETC/HOSTS FILE"
+echo "-------------------------------------------------------------------------------------------------------------------------"
 echo "192.168.16.21 server1" | sudo tee -a /etc/hosts
 
 # Software Installation
 echo "wE WILL NOY INSTALL APACHE AND SQUID "
+echo "---------------------------------------------------------------------------------------------------------------------------------"
 sudo apt update
 sudo apt install -y apache2 squid
 
 # Firewall Configuration
 echo "WE WILL NOW SET UP THE UFW FIREWALL"
+echo "-----------------------------------------------------------------------------------------------------------------------------------------------"
 sudo ufw enable
 sudo ufw allow 22
 sudo ufw allow 80
@@ -35,6 +40,7 @@ sudo ufw allow 3128
 
 # User Account Management
 echo "WE WILL NOW CREATE THE USER ACCOUNTS "
+echo "--------------------------------------------------------------------------------------------------------------------------------------"
 for user in dennis aubrey captain snibbles brownie scooter sandy perrier cindy tiger yoda; do
   sudo adduser --gecos "" --disabled-password $user
   if [ $user = "dennis" ]; then
@@ -43,5 +49,5 @@ for user in dennis aubrey captain snibbles brownie scooter sandy perrier cindy t
     echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG4rT3vTt99Ox5kndS4HmgTrKBT8SKzhK4rhGkEVGlCI student@generic-vm" | sudo tee /home/$user/.ssh/authorized_keys
   fi
 done
-
+echo "-----------------------------------------------------------------------------------------------------------------------------------------"
 echo " ALL CONFIGURATION DONE "
