a6e052d8b3335f564e53d1c2670765b7a9612ca9
shreypatel1702 <shreypatel7265@gmail.com>
2024-03-26 22:21:53 -0400

    HEllo

---
+++ error.sh
@@ -0,0 +1,8 @@
+shrey@Generic-VM:~/Linux-Automation$ sudo ovs-vsctl show
+87a93733-3ed4-4b85-b6a0-efa1d15b4899
+    Bridge "local:openwrt"
+        Port "local:openwrt"
+            Interface "local:openwrt"
+                type: internal
+                error: "could not add network device local:openwrt to ofproto (Invalid argument)"
+    ovs_version: "2.17.9"
