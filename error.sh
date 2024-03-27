a8f393bd849c2d23bf5c0d53e2a2d23c33bc0e4a
shreypatel1702 <shreypatel7265@gmail.com>
2024-03-26 23:03:06 -0400

    HEllo

--- error.sh
+++ error.sh
@@ -1,66 +1,23 @@
-hrey@Generic-VM:~/Linux-Automation$ sudo bash assignment2.sh 
+shrey@Generic-VM:~/Linux-Automation$ sudo bash assignment2.sh 
 assignment2.sh: line 2: $'\r': command not found
-assignment2.sh: line 4: $'\r': command not found
 WE WILL NOW CONFIGURE NETWORK CONFIGURATION
 --------------------------------------------------------------------------------------------------------------------------------
 bash: line 13: warning: here-document at line 1 delimited by end-of-file (wanted `EOF')
 usage: /usr/sbin/netplan  [-h] [--debug]  ...
 /usr/sbin/netplan : error: argument : invalid choice: 'apply\r' (choose from 'help', 'apply', 'generate', 'get', 'info', 'ip', 'set', 'rebind', 'status', 'try')
-assignment2.sh: line 21: $'\r': command not found
+assignment2.sh: line 20: $'\r': command not found
 WE WILL NOW UPDATE /ETC/HOSTS FILE
 -------------------------------------------------------------------------------------------------------------------------
-192.168.16.21 server1
 assignment2.sh: line 26: $'\r': command not found
-wE WILL NOY INSTALL APACHE AND SQUID 
+WE WILL NOW INSTALL APACHE AND SQUID 
 ---------------------------------------------------------------------------------------------------------------------------------
 E: Invalid operation update
 Reading package lists... Done
 Building dependency tree... Done
 Reading state information... Done
 E: Unable to locate package squid
-assignment2.sh: line 32: $'\r': command not found
-WE WILL NOW SET UP THE UFW FIREWALL
------------------------------------------------------------------------------------------------------------------------------------------------
-ERROR: Invalid syntax
-
-Usage: ufw COMMAND
-
-Commands:
- enable                          enables the firewall
- disable                         disables the firewall
- default ARG                     set default policy
- logging LEVEL                   set logging to LEVEL
- allow ARGS                      add allow rule
- deny ARGS                       add deny rule
- reject ARGS                     add reject rule
- limit ARGS                      add limit rule
- delete RULE|NUM                 delete RULE
- insert NUM RULE                 insert RULE at NUM
- prepend RULE                    prepend RULE
- route RULE                      add route RULE
- route delete RULE|NUM           delete route RULE
- route insert NUM RULE           insert route RULE at NUM
- reload                          reload firewall
- reset                           reset firewall
- status                          show firewall status
- status numbered                 show firewall status as numbered list of RULES
- status verbose                  show verbose firewall status
- show ARG                        show firewall report
- version                         display version information
-
-Application profile commands:
- app list                        list application profiles
- app info PROFILE                show information on PROFILE
- app update PROFILE              update PROFILE
- app default ARG                 set default application policy
-
-ERROR: Bad port
-ERROR: Bad port
-ERROR: Bad port
-assignment2.sh: line 40: $'\r': command not found
-WE WILL NOW CREATE THE USER ACCOUNTS 
---------------------------------------------------------------------------------------------------------------------------------------
-assignment2.sh: line 44: syntax error near unexpected token `$'do\r''
-'ssignment2.sh: line 44: `for user in dennis aubrey captain snibbles brownie scooter sandy perrier cindy tiger yoda; do
+assignment2.sh: line 53: syntax error near unexpected token `$'do\r''
+'ssignment2.sh: line 53: `for user in "${users[@]}"; do
 shrey@Generic-VM:~/Linux-Automation$ 
+
 
