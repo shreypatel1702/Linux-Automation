94af1da8b3260ddb98ac26cdd586a7a46515e3d1
shreypatel1702 <shreypatel7265@gmail.com>
2024-03-28 12:29:09 -0400

    Hello

--- error.sh
+++ error.sh
@@ -118,3 +118,27 @@ chmod +x ./check-assign2-script.sh &&
 git add -A &&
 git commit -m "Assignmment2 checked" &&
 git push
+
+
+
+
+
+
+
+
+
+shrey@Generic-VM:~/Linux-Automation$ cd ~/Linux-Automation && wget -O check-assign2-script.sh https://zonzorp.github.io/COMP2137/check-assign2-script.sh && chmod +x ./check-assign2-script.sh && ./check-assign2-script.sh https://github.com/shreypatel1702/Linux-Automation/assignment2.sh 2>&1 >assignment2-checked.txt && git add -A && git commit -m "Assignmment2 checked" && git push
+--2024-03-28 12:28:00--  https://zonzorp.github.io/COMP2137/check-assign2-script.sh
+Resolving zonzorp.github.io (zonzorp.github.io)... 185.199.111.153, 185.199.109.153, 185.199.110.153, ...
+Connecting to zonzorp.github.io (zonzorp.github.io)|185.199.111.153|:443... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: 2236 (2.2K) [application/x-sh]
+Saving to: ‘check-assign2-script.sh’
+
+check-assign2-script.sh          100%[==========================================================>]   2.18K  --.-KB/s    in 0s      
+
+2024-03-28 12:28:00 (22.8 MB/s) - ‘check-assign2-script.sh’ saved [2236/2236]
+
+Error: Failed loading network: Network not found
+Error: Failed loading network: Network not found
+shrey@Generic-VM:~/Linux-Automation$ 
