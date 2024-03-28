c2707b35c03995b5894e635ec8ea2e8324d0cc11
shreypatel1702 <157723420+shreypatel1702@users.noreply.github.com>
2024-03-28 12:19:22 -0400

    Update error.sh

--- error.sh
+++ error.sh
@@ -105,3 +105,16 @@ chmod +x ~/check-assign2-script.sh &&
 Review the rubric linked above for details on how your work will be graded.
 
 You are done with the learning activity for this lesson and should proceed to writing the quiz for this lesson before the next class in this course begins.
+
+
+
+
+
+
+cd ~/repositoryname &&
+wget -O check-assign2-script.sh https://zonzorp.github.io/COMP2137/check-assign2-script.sh &&
+chmod +x ./check-assign2-script.sh &&
+./check-assign2-script.sh https://github.com/yourgithubusername/yourreponame/assignment2.sh 2>&1 >assignment2-checked.txt &&
+git add -A &&
+git commit -m "Assignmment2 checked" &&
+git push
