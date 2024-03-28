107555a560d383d00eb23f936a3016501fc3bece
shreypatel1702 <shreypatel7265@gmail.com>
2024-03-28 01:43:20 -0400

    Hello

--- Demo.sh
+++ Demo.sh
@@ -1,19 +1,22 @@
-shrey@Generic-VM:~/Linux-Automation$ wget -O ~/check-assign2-script.sh https://zonzorp.github.io/COMP2137/check-assign2-script.sh && 
+hrey@Generic-VM:~/Linux-Automation$ wget -O ~/check-assign2-script.sh https://zonzorp.github.io/COMP2137/check-assign2-script.sh && 
 
 chmod +x ~/check-assign2-script.sh &&
 
-~/check-assign2-script.sh https://github.com/yourgithubusername/yourreponame/assignment2.sh 2>&1 >~/assignment2-checked.txt
+~/check-assign2-script.sh https://github.com/shreypatel1702/Linux-Automation/assignment2.sh 2>&1 >~/assignment2-checked.txt
 
---2024-03-26 22:06:38--  https://zonzorp.github.io/COMP2137/check-assign2-script.sh
-Resolving zonzorp.github.io (zonzorp.github.io)... 185.199.109.153, 185.199.108.153, 185.199.111.153, ...
-Connecting to zonzorp.github.io (zonzorp.github.io)|185.199.109.153|:443... connected.
+--2024-03-28 01:39:19--  https://zonzorp.github.io/COMP2137/check-assign2-script.sh
+Resolving zonzorp.github.io (zonzorp.github.io)... 185.199.111.153, 185.199.108.153, 185.199.109.153, ...
+Connecting to zonzorp.github.io (zonzorp.github.io)|185.199.111.153|:443... connected.
 HTTP request sent, awaiting response... 200 OK
 Length: 2236 (2.2K) [application/x-sh]
 Saving to: ‘/home/shrey/check-assign2-script.sh’
 
-/home/shrey/check-assign2-script 100%[==========================================================>]   2.18K  --.-KB/s    in 0.003s  
+/home/shrey/check-assign2-script 100%[==========================================================>]   2.18K  --.-KB/s    in 0.001s  
 
-2024-03-26 22:06:38 (641 KB/s) - ‘/home/shrey/check-assign2-script.sh’ saved [2236/2236]
+2024-03-28 01:39:19 (1.86 MB/s) - ‘/home/shrey/check-assign2-script.sh’ saved [2236/2236]
 
-Error: Failed checking instance exists "local:openwrt": Instance not found
-WARNING:root:Cannot call Open vSwitch: ovsdb-server.service is not running.
+/etc/netplan/50-cloud-init.yaml:11:24: Invalid YAML: inconsistent indentation:
+            nameservers:
+                       ^
+^C
+shrey@Generic-VM:~/Linux-Automation$ 
