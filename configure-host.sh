f928a824d6493b383706d82e6a8355394750a794
shreypatel1702 <157723420+shreypatel1702@users.noreply.github.com>
2024-04-04 16:13:43 -0400

    Update configure-host.sh

--- configure-host.sh
+++ configure-host.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
-# Default values
 desired_name=""
 desired_ip=""
 desired_host=""
 desired_host_ip=""
 
-# Parse command line arguments
+
+# NOW WE WILL UPDATE HOSTNAME-------------------------------------------------------------------------------------------
+if [ "$desired_name" != "" ]; then
+    hostnamectl set-hostname "$desired_name"
+fi
+
+# NOW WE WILL UPDATE HOSTT-------------------------------------------------------------------------------------------------
+if [ "$desired_host" != "" ] && [ "$desired_host_ip" != "" ]; then
+    echo "$desired_host_ip\t$desired_host" >> /etc/hosts
+fi
+
+# NOW WE WILL UPDATE IP ADDRESSS-------------------------------------------------------------------------------------------------------
+if [ "$desired_ip" != "" ]; then
+    sed -i "/^address/c\    address $desired_ip/32" /etc/netplan/01-netcfg.yaml
+    sed -i "/^hosts/c\\$desired_ip\t$desired_name" /etc/hosts
+    netplan apply
+fi
+
+
+# HERE IS ALL ARGUEMENTS-------------------------------------------------------------------------------------------------------------
 while [ $# -gt 0 ]; do
     case "$1" in
         -name)
@@ -33,19 +51,5 @@ while [ $# -gt 0 ]; do
     esac
 done
 
-# Update hostname
-if [ "$desired_name" != "" ]; then
-    hostnamectl set-hostname "$desired_name"
-fi
 
-# Update IP address
-if [ "$desired_ip" != "" ]; then
-    sed -i "/^address/c\    address $desired_ip/32" /etc/netplan/01-netcfg.yaml
-    sed -i "/^hosts/c\\$desired_ip\t$desired_name" /etc/hosts
-    netplan apply
-fi
-
-# Update host entry
-if [ "$desired_host" != "" ] && [ "$desired_host_ip" != "" ]; then
-    echo "$desired_host_ip\t$desired_host" >> /etc/hosts
-fi
+#ALL CONFIGURATIONS DONE---------------------------------------------------------------------------------------------------------------------------------
